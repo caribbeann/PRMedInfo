abc8de4f3a9868038edf5a982bf3cbe8befa577b
Stefan Oancea <oancea.stefan@icloud.com>
2017-10-25 18:45:48 +0200

    initial sanity check: everything works!

---
+++ test.py
@@ -0,0 +1,20 @@
+from openmesh import *
+
+mesh = TriMesh()
+
+read_mesh(mesh, r"image data\lowerJawMesh.obj")
+
+write_mesh(mesh,  r"image data\lowerJawMesh_test.obj")
+mesh = TriMesh()
+mesh.request_halfedge_normals()
+mesh.request_vertex_normals()
+
+options = Options()
+options += Options.VertexNormal
+
+result = read_mesh(mesh, r"image data\lowerJawMesh_test.obj", options)
+
+if result:
+       print "everything worked"
+else:
+       print "something went wrong"
