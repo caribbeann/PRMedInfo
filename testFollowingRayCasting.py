e03649607c3267c02400d99d0059b368be473183
flogtk <gtkflo@gmail.com>
2017-11-03 16:40:28 +0100

    code cleanup tests raycasting and raycasting postprocessing

--- testFollowingRayCasting.py
+++ testFollowingRayCasting.py
@@ -5,25 +5,35 @@ import renderer
 import raycasting
 import smoothing
 
-# Create polydata to slice the grid with. In this case, use a cone. This could
-# be any polydata including a stl file.
+
+################## Part 1 : Preprocessing ########################
+
+### Load data, find principal components and reorient the data according to these principal components axes
+### The first component will be the x axis and corresponds to the left-right axis
+### The second component will be the y axis and corresponds to the anteroposterior axis
+### The last component will be the z axis and corresponds to the craniocauda axis
+### Then crop the mesh to get rid of the tongue/palatin, to ease the computation
+
+# Load initial mesh
 reader = vtk.vtkOBJReader()
-
 reader.SetFileName("lowerJawMesh.obj")
-
 reader.Update()
-
-reader2 = vtk.vtkOBJReader()
-
-reader2.SetFileName("suggest_alveolarRidgeLine_lower_finalVisualization.obj")
-
-reader2.Update()
-
-
-
 polydata = reader.GetOutput()
 
-#Preprocessing : rotate data and find center of gravity
+# Load suggested alveolar line for comparison purposes
+reader2 = vtk.vtkOBJReader()
+reader2.SetFileName("suggest_alveolarRidgeLine_lower_finalVisualization.obj")
+reader2.Update()
+suggest = reader2.GetOutput() 
+
+# Load computed alveolar line
+reader3 = vtk.vtkPolyDataReader()
+reader3.SetFileName("./rayCastLower360.vtk")
+reader3.Update()
+rays = reader3.GetOutput()
+
+
+# Reorient data
 meshOp = meshOperations.MeshOperations(poly_data=polydata)
 PCADict = meshOp.computePCA()
 polydata = meshOp.rotate([0.0,1.0,0.0],PCADict['eigenvectors'][1])
@@ -38,8 +48,7 @@ meshOp.changePolyData(polydata)
 polydata = meshOp.translate(translation[0],translation[1],translation[2])
 
 
-suggest = reader2.GetOutput() 
-
+# Reorient suggest to match the reoriented data
 meshOp2 = meshOperations.MeshOperations(poly_data=suggest)
 suggest = meshOp2.rotate([0.0,1.0,0.0],PCADict['eigenvectors'][1])
 
@@ -49,6 +58,8 @@ suggest = meshOp2.rotate([0.0,0.0,1.0],PCADict2['eigenvectors'][2])
 meshOp2.changePolyData(suggest)
 suggest = meshOp2.translate(translation[0],translation[1],translation[2])
 
+
+# Compute center of mass
 bounds = polydata.GetBounds()
 
 meshOp.changePolyData(polydata)
@@ -59,18 +70,13 @@ suggest = meshOp.translate(-x,-y,-z)
 
 
 
+################## Part 2 : Smoothing ########################
 
-#Open found points
-reader3 = vtk.vtkPolyDataReader()
-reader3.SetFileName("./rayCastLower360.vtk")
-reader3.Update()
-
-rays = reader3.GetOutput()
-
-
-rend=renderer.Renderer(poly_data=rays, wire_frame=False)
-rend.render()
-
+### The computed polyline is very noisy, there is a need to smooth it
+### Here we bring closer the points which are too far from each other, in order to have a smoother line
+### Then, we compute the distance between each smoothed point and the corresponding first computed polyline point
+### The new point's location will be a weighted position between the smooth and noisy initial point
+### These new positions might be a bit noisy, they will be smoothed again
 
 # Smooth and weight smoothed with firstTest, then smooth again and find points on mesh
 smth = smoothing.Smoothing(poly_data=rays)
@@ -79,15 +85,15 @@ smth.changePolyData(rays)
 alv_line_points = smth.smooth(50,0.1) #smooth again (but less) => points are NOT on the mesh but have right curve
 
 
-# find intersection of vertical lines with the original aligned mesh
+################## Part 3 : Postprocessing ########################
 
+### The smoothed line points are currently not on the mesh (as they are interpolations), we need to project them on the mesh
+
+# Find intersection of vertical lines with the original mesh
 original_locator = vtk.vtkOBBTree()
 original_locator.SetDataSet(polydata)
 original_locator.BuildLocator()
 
-cells_original = vtk.vtkIdList()
-
-line_length = (polydata.GetBounds()[5]-polydata.GetBounds()[4]) + 1
 bounds = polydata.GetBounds()
 
 skeleton_points_final = vtk.vtkPoints() # here we add the points that we find
@@ -119,13 +125,17 @@ for alv_line_point in alv_line_points:
 poly_line_final.GetPointIds().SetNumberOfIds(counter-1)
 whole_line_final.InsertNextCell(poly_line_final)
 
+# Build polydata
 skeleton_final = vtk.vtkPolyData()
 skeleton_final.SetPoints(skeleton_points_final)
 skeleton_final.SetLines(whole_line_final)
 
 
 
-#Visualization stuff
+#################### Part 4 : Visualization ########################
+
+### Visualize the jaw mesh, as well as the computed alveolar line (white) and the suggested alveolar line (red)
+
 inputMapper = vtk.vtkDataSetMapper()
 planeMapper = vtk.vtkDataSetMapper()
 if vtk.VTK_MAJOR_VERSION <= 5:
